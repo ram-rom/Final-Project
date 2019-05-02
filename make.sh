b554e3c2e94065dce1a59fd83fd867bc283820b9
jpoppler <jason@getnotion.com>
2019-05-01 23:06:10 -0600

    Fix a few issues.
    Add install to the top of the script as its easier to have the script do
    it than ask others to. Also fix bug where it the directory to the plan
    files was changed.

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TEST="${SCRIPT_DIR}/tests"
 
 # Notify user that previous analysis and plans will be removed
-echo "WARNING: Running this script will remove the plans and analyze folders, if they exist."
+echo "WARNING: Running this script will remove simplescalar directory, the plans and analyze folders, if they exist."
 echo "Would you like to continue? Please enter y for yes or n for no followed by [ENTER]:"
 read response
 
@@ -12,8 +12,12 @@ if [ "$response" != "y" ]; then
   exit 0
 fi
 
+rm -rf $SCRIPT_DIR/simplescalar
 rm -rf $SCRIPT_DIR/plans
 rm -rf $SCRIPT_DIR/analyze
+
+# install simple scalar
+./bin/ram-rom --install
 
 # Check for pry. This can cause errors
 if ! gem spec pry > /dev/null 2>&1; then
@@ -21,21 +25,22 @@ if ! gem spec pry > /dev/null 2>&1; then
 fi
 counter=0
 # Run plan.yml files
-for order in tests/plans/*; do
-    # Remove soft link for sim out of order file
-    rm -rf $SCRIPT_DIR/simplescalar/simplesim-3.0/sim-outorder.c
-    # Add link to current sim-outorder.c file
-    ln -s $TEST/test_src/sim-outorder-$counter.c $SCRIPT_DIR/simplescalar/simplesim-3.0/sim-outorder.c
-    # Compile
-    ./bin/ram-rom --compile
-    # Check for simpel-test completion
-    OUTPUT="$(./bin/ram-rom --simple-test perceptron | tail -1)"
-    if [ $OUTPUT != "Completed" ]; then
-        echo "It looks like simple-test failed. Check the compiltation for errors and fix them."
-        exit -1
-    fi
-    ./bin/ram-rom --plan $order
-    let "counter++"
+for order in tests/plan_files/*; do
+   # Remove soft link for sim out of order file
+   rm -rf $SCRIPT_DIR/simplescalar/simplesim-3.0/sim-outorder.c
+   # Add link to current sim-outorder.c file
+   ln -s $TEST/test_src/sim-outorder-$counter.c $SCRIPT_DIR/simplescalar/simplesim-3.0/sim-outorder.c
+   # Compile
+   ./bin/ram-rom --compile
+   # Check for simpel-test completion
+   OUTPUT="$(./bin/ram-rom --simple-test perceptron | tail -1)"
+   if [ $OUTPUT != "Completed" ]; then
+       echo "It looks like simple-test failed. Check the compiltation for errors and fix them."
+       exit -1
+   fi
+   echo "*** $order ***"
+   ./bin/ram-rom --plan $order
+   let "counter++"
 done
 #Run analyze
 ./bin/ram-rom --analyze
